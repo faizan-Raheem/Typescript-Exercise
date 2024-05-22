2a36c0fa9dab00e57856b55092b9b2974ca63209
faizan-Raheem <faizankhan71464@gmail.com>
2024-05-22 09:57:53 -0400

    Add files via upload

---
+++ guest_list.ts.ts
@@ -0,0 +1,8 @@
+
+
+let Guest_list :string[] = ['umair','mubeen','osama',] ;
+
+for(let i=0; i<Guest_list.length; i++){
+
+    console.log('dear mr. '+Guest_list[i] +',\n\nit is our pleasure to invite you in our party.\n\nthank you!')
+}
