64081595d0fe02b57b54649167a180b6cd61c00c
faizan-Raheem <faizankhan71464@gmail.com>
2024-05-22 09:59:50 -0400

    Add files via upload

--- guest_list.ts.ts
+++ guest_list.ts.ts
@@ -2,7 +2,35 @@
 
 let Guest_list :string[] = ['umair','mubeen','osama',] ;
 
+// for(let i=0; i<Guest_list.length; i++){
+
+//     console.log('dear mr.' + Guest_list[i] + ',\n\nit is our pleasure to invite you in our party.\n\nthank you!')
+// }
+
+let absent_guest :string = 'umair' ;
+
+let new_guest :string = 'kamran tessori' ;
+
+Guest_list[0] = new_guest ;
+
 for(let i=0; i<Guest_list.length; i++){
 
-    console.log('dear mr. '+Guest_list[i] +',\n\nit is our pleasure to invite you in our party.\n\nthank you!')
-}
+    console.log('dear mr. ' + Guest_list[i] + ',\n\nit is our pleasure to invite you in our party.\n\nthank you!')
+
+}    
+    console.log(`mr. ${absent_guest} is not comming to the party`);
+
+    console.log('Good News! we find big table so we are inviting 3 more guests.')
+
+    Guest_list.unshift('sir zia khan') ;
+
+    Guest_list.splice(2 , 0 ,'maryam nawaz') ;
+
+    Guest_list.push('bilawal bhutto zardari');
+
+for(let i=0; i<Guest_list.length; i++){
+
+    console.log('dear mr. ' + Guest_list[i] + ',\n\nit is our pleasure to invite you in our party.\n\nthank you!')
+
+} 
+
