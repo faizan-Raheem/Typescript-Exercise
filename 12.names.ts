d41b61ca726845b8941bce3e18bf2a2eb64c18b6
faizan-Raheem <faizankhan71464@gmail.com>
2024-05-22 10:38:52 -0400

    Add files via upload

---
+++ 12.names.ts
@@ -0,0 +1,12 @@
+
+
+let names :string[] = ['faizan','umair','mubeen','samad'] ;
+
+const message :string =  'welcome to typescript class;  '
+
+
+for(let i=0; i<names.length; i++){
+
+    console.log(message + names[i]) ;
+    
+}
