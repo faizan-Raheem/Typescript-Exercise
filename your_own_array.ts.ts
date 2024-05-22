98dbf5e8e6424e422a37fd9e207489b928ba7f07
faizan-Raheem <faizankhan71464@gmail.com>
2024-05-22 10:16:57 -0400

    Delete your_own_array.ts.ts

--- your_own_array.ts.ts
+++
@@ -1,8 +0,0 @@
-
-
-let transportation :string[] = ['motorcycle','car','train','cycle'] ;
-
-for(let i=0; i<transportation.length; i++){
-
-    console.log('i would like to own a ' + transportation[i])
-}
