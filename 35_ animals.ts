95622a75e3327bcb166be299b367993e0c93c701
faizan-Raheem <faizankhan71464@gmail.com>
2024-05-30 09:25:53 -0400

    Add files via upload

---
+++ 35_ animals.ts
@@ -0,0 +1,7 @@
+let animals: string[] = ['dog','cat','rabit'];
+
+for(let animal of animals);{
+    console.log(`A ${animals} would make a great pet`);
+}
+
+console.log(`any of these animals would make a great pet!`);
