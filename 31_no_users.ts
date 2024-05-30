9ad670b1eaee337c5be2ef571b8c8360f948733a
faizan-Raheem <faizankhan71464@gmail.com>
2024-05-30 09:23:32 -0400

    Add files via upload

---
+++ 31_no_users.ts
@@ -0,0 +1,8 @@
+let userNames: string [] = ['admin','umair','ilyas','mubeen','faizan'];
+
+if(userNames.length === 0){
+    console.log('we need to find some users!');
+}else{
+    userNames = [];
+    console.log('all user name have been removed .' + userNames.length);
+}
