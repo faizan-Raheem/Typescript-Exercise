0ccee06d44c6d04c1c0113956cefe90076e2a5eb
faizan-Raheem <faizankhan71464@gmail.com>
2024-05-30 09:27:14 -0400

    Add files via upload

---
+++ 38_cities.ts
@@ -0,0 +1,8 @@
+function describe_city(city:string, country:string ='default country.'){
+    console.log(` ${city}is in ${country}`);
+}
+describe_city('karachi','pakistan');
+
+describe_city('tokyo','japan');
+
+describe_city('paris');
