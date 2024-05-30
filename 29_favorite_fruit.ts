1852ab9b104e9bfbbe7d94dced90af0613e217e6
faizan-Raheem <faizankhan71464@gmail.com>
2024-05-30 09:19:35 -0400

    Add files via upload

---
+++ 29_favorite_fruit.ts
@@ -0,0 +1,24 @@
+
+
+let favorite_fruits: string[] = ['apple','banana','mango'];
+
+if(favorite_fruits.includes('apple')){
+    console.log('i really like apple!');
+}
+
+
+if(favorite_fruits.includes('banana')){
+    console.log('i really like banana!');
+}
+
+if(favorite_fruits.includes('mango')){
+    console.log('i really like mango!');
+}
+
+if(favorite_fruits.includes('strawberry')){
+    console.log('i really like strawberries!');
+}
+
+if(favorite_fruits.includes('orrange')){
+    console.log('i really like orrange!');
+}
