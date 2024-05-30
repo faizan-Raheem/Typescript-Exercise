f0e791bde9966dc38ac9c0e95e561e7109b3b436
faizan-Raheem <faizankhan71464@gmail.com>
2024-05-30 09:25:21 -0400

    Add files via upload

---
+++ 34_pizzas.ts
@@ -0,0 +1,9 @@
+import { Console } from "console";
+
+let pizzas: string[] = ['pepparono','cuban','BBQ chicken'];
+
+for(let pizza of pizzas){
+    console.log(`i like ${pizza} pizza.`);
+}
+console.log();(`i really love pizza`);
+
