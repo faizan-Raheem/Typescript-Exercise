b9c7f79e6ef5196114712efe3e4b1cffae483dfd
faizan-Raheem <faizankhan71464@gmail.com>
2024-05-30 09:13:26 -0400

    Add files via upload

---
+++ 23.conditional_tests.ts
@@ -0,0 +1,57 @@
+
+// write a series of conditional tests.
+
+// creating car varible
+let car :string = 'subaru' ;
+// test no 1
+console.log("is car === 'subaru'? i predict true.");
+console.log(car === 'subaru');
+
+// test no 2
+console.log("is car == 'subaru'? i predict true.");
+console.log(car == 'subaru');
+
+// test no 3
+console.log("is car != 'honda'? i predict true.");
+console.log(car != 'honda');
+
+// test no 4
+console.log("is car !== 'ford'? i predict true.");
+console.log(car !== 'ford');
+
+// test nO 5
+console.log("is car. upper case == 'SUBARU'? i predict true.");
+console.log(car.toUpperCase()== 'SUBARU');
+
+// test nO 6
+console.log("is car  == 'SUBARU'? i predict false.");
+console.log(car == 'SUBARU');
+
+// test nO 7
+console.log("is car  === 'SUBARU'? i predict false.");
+console.log(car === 'SUBARU');
+
+// test nO 8
+console.log("is car  === 'train'? i predict false.");
+console.log(car === 'train');
+
+// test nO 9
+console.log("is car  === 'bus'? i predict false.");
+console.log(car === 'bus');
+
+// test nO 10
+console.log("is car  === 'cycle'? i predict false.");
+console.log(car === 'cycle');
+
+
+
+
+
+
+
+
+
+
+
+
+
