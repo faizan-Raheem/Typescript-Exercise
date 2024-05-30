f246f2114a21e7059db5a514fa18db31b420fbe5
faizan-Raheem <faizankhan71464@gmail.com>
2024-05-30 09:26:14 -0400

    Add files via upload

---
+++ 36_t_shirt.ts
@@ -0,0 +1,9 @@
+function make_shirt(size:string, text: string){
+
+    console.log(`creating a larg shirt with the message: ${text}`);
+}
+make_shirt('larg','hello world');
+
+make_shirt('medium','hello world');
+
+make_shirt('small','hello world');
