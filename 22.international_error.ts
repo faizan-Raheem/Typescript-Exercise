54062e691ba2f8417e9ea3f889a4d0ecdb1f7fc9
faizan-Raheem <faizankhan71464@gmail.com>
2024-05-30 09:12:10 -0400

    Add files via upload

---
+++ 22.international_error.ts
@@ -0,0 +1,9 @@
+
+// creating array
+let newArray  :string[] = ['will smith', 'james bond', 'tom cruise'];
+// accessing array by index
+console.log(newArray[5]);
+
+// make sureyou correct the error before closing the program.
+// correct the error
+console.log(newArray);
