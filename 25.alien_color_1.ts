8b6d0850e4d2e1b8de61a915efde8cbbb1dbd55a
faizan-Raheem <faizankhan71464@gmail.com>
2024-05-30 09:15:20 -0400

    Add files via upload

---
+++ 25.alien_color_1.ts
@@ -0,0 +1,13 @@
+
+// creat a varible called aliean_color
+let aliean_color :string = 'green';
+
+if(aliean_color === 'green'){
+console.log("player just earned 5 points  !");
+}
+
+ aliean_color  = 'yellow';
+// the version that fails will have no output
+if(aliean_color === 'green'){
+    console.log("player just earned 5 points  !");
+}
