1d92326710b4d301b347db7595473bbe18459124
faizan-Raheem <faizankhan71464@gmail.com>
2024-05-30 09:16:45 -0400

    Add files via upload

---
+++ 26_aliean_color_2.ts
@@ -0,0 +1,18 @@
+import {Console} from "console";
+
+let alieanColors: string = "green";
+
+
+if(alieanColors === "green"){
+console.log("player just earned 5 points for shooting the aliean.");
+}else{
+    console.log("player just earned 10 point.")
+}
+
+alieanColors = "yello"
+if(alieanColors === "green"){
+    console.log("player just earned 5 points for shooting the aliean.");
+    }else{
+        console.log("player just earned 10 point.")
+    }
+
