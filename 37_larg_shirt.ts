35fbbacf4ebff4bd12dbde277d78e78077e037ea
faizan-Raheem <faizankhan71464@gmail.com>
2024-05-30 09:26:39 -0400

    Add files via upload

---
+++ 37_larg_shirt.ts
@@ -0,0 +1,6 @@
+function make_shirt(size:string = 'larg', text:string ='i love typescript.'){
+    console.log(`creating a ${size} shirt with the message: ${text}`);
+}
+make_shirt();
+make_shirt('medium');
+make_shirt ('small', 'i love python');
